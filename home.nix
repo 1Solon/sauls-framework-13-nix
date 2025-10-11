8736b60c9029a9b284384cba19eb0ae1b71a1534
1Solon <Solonerus@gmail.com>
2025-10-11 16:17:50 +0100

    Moves zsh config to seperate file

--- home.nix
+++ home.nix
@@ -15,6 +15,7 @@
     ./programs/zen.nix
     ./programs/git.nix
     ./programs/github-cli.nix
+    ./programs/zsh.nix
     inputs.zen-browser.homeModules.beta
   ];
 
@@ -60,21 +61,6 @@
     # Communication
     discord-ptb
   ];
-
-  # Zsh configuration
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-
-    shellAliases = {
-      update = "nix flake update --flake /home/saul/sauls-framework-13-nixos && sudo nixos-rebuild switch --impure --flake /home/saul/sauls-framework-13-nixos#sauls-laptop";
-      config = "code /home/saul/sauls-framework-13-nixos";
-      init-kube = "mkdir -p ~/.kube && op document get \"kubeconfig\" > ~/.kube/config";
-      init-talos = "mkdir -p ~/.talos && op document get \"talosconfig\" > ~/.talos/config";
-    };
-  };
 
   # Starship prompt configuration
   programs.starship = {
---
+++ programs/zsh.nix
@@ -0,0 +1,17 @@
+{ ... }:
+{
+  # Zsh configuration
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      update = "nix flake update --flake /home/saul/sauls-framework-13-nixos && sudo nixos-rebuild switch --impure --flake /home/saul/sauls-framework-13-nixos#sauls-laptop";
+      config = "code /home/saul/sauls-framework-13-nixos";
+      init-kube = "mkdir -p ~/.kube && op document get \"kubeconfig\" > ~/.kube/config";
+      init-talos = "mkdir -p ~/.talos && op document get \"talosconfig\" > ~/.talos/config";
+    };
+  };
+}
