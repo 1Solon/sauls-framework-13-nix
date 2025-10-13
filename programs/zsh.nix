a47e737d1ea9253104db26a4cede6e9eb133a157
1Solon <Solonerus@gmail.com>
2025-10-13 20:16:24 +0100

    Adds switch and hyprland config

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -7,7 +7,8 @@
     syntaxHighlighting.enable = true;
 
     shellAliases = {
-      update = "nix flake update --flake /home/saul/sauls-framework-13-nixos && sudo nixos-rebuild switch --impure --flake /home/saul/sauls-framework-13-nixos#sauls-laptop";
+      update = "nix flake update --flake /home/saul/sauls-framework-13-nixos";
+      switch = "sudo nixos-rebuild switch --impure --flake /home/saul/sauls-framework-13-nixos#sauls-laptop";
       config = "code /home/saul/sauls-framework-13-nixos";
       init-kube = "mkdir -p ~/.kube && op document get \"kubeconfig\" > ~/.kube/config";
       init-talos = "mkdir -p ~/.talos && op document get \"talosconfig\" > ~/.talos/config";
